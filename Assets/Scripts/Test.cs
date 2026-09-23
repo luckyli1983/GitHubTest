7a901aae7e22cfdd3867b5f9ece761b8919afc39
luckyli1983 <luckyli1983@qq.com>
2026-09-23 23:26:41 +0800

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
+        Debug.Log("Hello, World! V1.0");
     }
 
     // Update is called once per frame
